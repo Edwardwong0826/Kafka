2f2d6031db99d8a3d60b9bbe281081e4e772a30e
Edwardwong0826 <edwardwong_94@outlook.com>
2024-04-23 16:30:58 +0800

    Kafka Consumer
    Add info about Kafka consumer partition assign strategy, how Kafka elect the leader for the consumer group

--- src/main/java/org/wong/kafka/test/consumer/Consumer.java
+++ src/main/java/org/wong/kafka/test/consumer/Consumer.java
@@ -1,9 +1,6 @@
 package org.wong.kafka.test.consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -21,8 +18,28 @@ public class Consumer {
         // is ok that consumer group to have more consumer than topic partition, if one of the consumer is down, the idle consumer can act as backup to consume
         // kafka server will store group consumer offset into internal topic called _consumer_offsets, _consumer_offsets inside also split into 50 partitions
         // so same group backup consumer can continue consumed back from last time same group down consumer offset
-        // internally, kafka borker server use GroupCoordinatorAdapter exchange with kafka server ConsumerCoordinator to do store group consume offset into _consumer_offsets partitions
+        // internally, kafka broker server use GroupCoordinatorAdapter exchange with kafka server ConsumerCoordinator to do store group consume offset into _consumer_offsets partitions
         // if consumer 1 and 2 is down, then consumer 0 will eventually consume from partition 0, 1, 2 simultaneously
+
+        // consumer partition assign strategy
+        // every consumer group got leader and follower, the leader is the first consume that join the group, and leader will decide consumer consume which from topic partition
+        // same group consumer subscribe to one same topic, so consumer group consumer can concurrent consume all data from topic
+        // in order to avoid data repeat consume, every topic each partition only will allow consumed by one of the consumer of the consumer group only
+        // means no two consumer or more consumer simultaneously consume data from same one partition data, instead one consumer allow to consume data from different partition
+        // consumer group consumer better do not more than topic partition quantity, because idle consumer cannot consume data, but can act as backup if wanted
+
+        // when consumer join the group and there is leader exist, it will send the join group request, GroupCoordinatorAdapter will kick existing leader and all exist consumer out of group and elect the new leader,
+        // consumers will simultaneously join the group and this time whoever consumer first join will become new leader (every consumer is possible to become leader)
+        // and leader responsible assign its follower to consume which partition
+        // above process will repeat when there is new consumer join the same consumer group
+
+        // there is common of four Kafka provided partition assign strategy, kafka default choose to use RangeAssignor and CooperativeStickyAssignor
+        // 1. RoundRobinAssignor strategy - Round robin topic partition to subscribe consumer, non subscribe consumer will be skipped
+        // 2. RangeAssignor strategy - according each topic partition number to calculate and assign, try to allocate average the partition to consumer as possible,
+        //    when cannot allocate average, then fill in the head sequentially
+        // 3. StickyAssignor strategy - after first assign, every consumer keep the assign partition information, if there is consumer leave and join, then redistribution
+        //    try to keep consumer original consume partition not change or less change
+        // 4. CooperativeStickyAssignor strategy - optimized StickyAssignor process of redistribution  with using cooperative protocol, this is more high performance to StickyAssignor
 
         Map<String, Object> configMap = new HashMap<>();
         configMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -30,12 +47,15 @@ public class Consumer {
         configMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
         configMap.put(ConsumerConfig.GROUP_ID_CONFIG, "wong");
+        configMap.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "aaa"); // consumer member id
+        configMap.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName()); // only the leader able to config
 
         // create consumer object
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configMap);
 
         // subscribe kafka topic
         consumer.subscribe(Collections.singletonList("test"));
+        //consumer.subscribe(Arrays.asList("test1","test2"));
 
         // get data from kafka topic
         // consumer pull data from kafka topic, it depends on its capability
